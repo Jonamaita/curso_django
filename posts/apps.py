1e2b1a18904263be5d189cd695684d842ec4846c
jonathan <jonamaita@gmail.com>
2020-12-07 13:39:54 -0300

    feat: add app posts. The list_posts view is like a cgi script

---
+++ posts/apps.py
@@ -0,0 +1,13 @@
+"""
+Posts application module
+"""
+from django.apps import AppConfig
+
+
+class PostsConfig(AppConfig):
+    """
+    Posts application settings.
+    """
+
+    name = "posts"
+    verbose_name = "Posts"
