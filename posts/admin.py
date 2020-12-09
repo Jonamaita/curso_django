cb77efcf84d3f179a0ec340f711f67c6bb608a51
jonathan <jonamaita@gmail.com>
2020-12-09 18:06:31 -0300

    feat: register admin to posts app

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from posts.models import Post
+
 # Register your models here.
+
+
+@admin.register(Post)
+class PostsAdmin(admin.ModelAdmin):
+    """
+    Posts admin
+    """
+
+    list_display = ("user", "title", "photo")
+    list_display_links = ("user",)
+
+    list_editable = ("title", "photo")
