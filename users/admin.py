944be53fca4e315c4b98d21c50c5814e99cdb0a8
jonathan <jonamaita@gmail.com>
2020-12-09 16:53:34 -0300

    feat: add ProfileInline class to User native class

--- users/admin.py
+++ users/admin.py
@@ -3,7 +3,10 @@ User admin classes.
 """
 
 from django.contrib import admin
+# Importamos UserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
+from django.contrib.auth.models import User
 from users.models import Profile
 
 #Por convencion la clase que creemos debe terminar en Admin.
@@ -40,3 +43,77 @@ class ProfileAdmin(admin.ModelAdmin):
         "created",
         "modified",
     )
+
+    # Nos desplagara los datos que deseamos. Es importante que la información
+    # este en tuplas.
+    fieldsets = (
+        (
+            "Profile", { # Nombre de la sección o titulo
+                "fields": ( # Los campos que visualizaremos.
+
+                        # Cuando declaramos varios campos en la misma tupla
+                        # se va a desplegar los datos en la misma fila.
+                        ("user", "picture"),
+                    ),
+                }
+        ),
+        (
+            "Extra info", {
+                "fields": (
+
+                        # En este caso la información se desplegara en 2 filas
+                        # ya que la tupla de fields tiene 2 tuplas.
+                        ("website", "phone_number"),
+                        ("biography"),
+                    ),
+            }
+        ),
+        (
+            "Meta data", {
+                "fields": (
+                    # Estos datos no se pueden modificar, ya que, en el modelo
+                    # se declaro que no son editables, por lo que haremos uso
+                    # de readonly_fields.
+                    ("created", "modified"),
+                )
+            }
+
+        ),
+    )
+    # Aqui declararemos los campos que solo pueden ser leidos pero no
+    # modificados.
+    readonly_fields = ("created", "modified")
+
+
+# Para incluir el módelo Profile en el módelo User, hacemos lo siguiente.
+class ProfileInline(admin.StackedInline):
+    """
+    En esta clase definiremos el modelo que deseamos asociar a User, en nuestro
+    caso Profile.
+    """
+
+    model = Profile
+    can_delete = False
+    verbose_name_plural = "profiles"
+
+# Luego para asociar los modelos e insertarlo en el Dashboard usaremos
+# el UserAdmin de Django el cual le dimos el alias de BaseUserAdmin.
+class UserAdmin(BaseUserAdmin):
+    """
+    Add Profile admin to base user admin.
+    """
+
+    inlines = (ProfileInline,)
+    list_display = ( # En list_display
+    "username",
+    "email",
+    "first_name",
+    "last_name",
+    "is_active",
+    "is_staff",
+  )
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+# Le pasamos el modelo y la clase que va a usar
+admin.site.register(User, UserAdmin)
