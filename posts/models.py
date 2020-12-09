de95b3adacce343868c2862777af6b82994df534
jonathan <jonamaita@gmail.com>
2020-12-08 23:47:25 -0300

    refactor: delete toy model user from posts app

--- posts/models.py
+++ posts/models.py
@@ -5,24 +5,3 @@ Models posts
 from django.db import models
 
 # Create your models here.
-
-class User(models.Model):
-    """
-    User Model
-    """
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=100)
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    bio = models.TextField(blank=True)
-    birthdate = models.DateField(blank=True, null=True)
-    created = models.DateTimeField(auto_now_add=True)
-    modified = models.DateTimeField(auto_now=True)
-    is_admin = models.BooleanField(default=False)
-
-
-    def __str__(self):
-        """
-        Return email
-        """
-        return self.email
