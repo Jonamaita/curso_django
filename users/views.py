514fc553a538c05178be1c1d22fa509e9f4e24ba
jonathan <jonamaita@gmail.com>
2020-12-10 12:21:04 -0300

    feat: add login view to users app

--- users/views.py
+++ users/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+"""
+User Views
+"""
+# Importamos authenticate y login
+from django.contrib.auth import authenticate, login
+# Redirect nos ayudara a redireccionarnos a otro path
+from django.shortcuts import redirect, render
 
-# Create your views here.
+
+def login_view(request):
+    """
+    login view
+    """
+
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        # El metodo authenticate tratara de contrastar el usuario con una
+        # instancia del modelo users que creamos.
+        user = authenticate(request, username=username, password=password)
+        if user:
+            # En caso de ser exitoso la autenticación creara un token de
+            # nuestro usuario para almacenarlo en memoria.
+            login(request, user)
+
+            # redireccionaremos al path con alias 'feed' que es para la url
+            # 'posts/'
+            return redirect("feed")
+
+        # En caso de dar false la autenticacion volveremos a renderizar el
+        # login, pero enviando la variable 'error'.
+        return render(
+            request,
+            "users/login.html",
+            {"error": "Invalid username and password"},
+        )
+
+    return render(request, "users/login.html")
