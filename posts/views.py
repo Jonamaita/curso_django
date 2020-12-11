1577ed061a059e8286de5084a99a05eb428aa752
jonathan <jonamaita@gmail.com>
2020-12-11 12:08:03 -0300

    reafactor: minor changes in lists_posts view in posts/views.py

--- posts/views.py
+++ posts/views.py
@@ -2,54 +2,26 @@
 Posts Views
 """
 
-from datetime import datetime
-
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
+# Models
+from posts.models import Post
 
 # Forms
 from posts.forms import PostForm
 
+
 # Create your views here.
-
-posts = [
-    {
-        "title": "Mont Blanc",
-        "user": {
-            "name": "Yésica Cortés",
-            "picture": "https://picsum.photos/60/60/?image=1027",
-        },
-        "timestamp": datetime.now().strftime("%b %dth, %Y - %H:%M hrs"),
-        "photo": "https://picsum.photos/800/600?image=1036",
-    },
-    {
-        "title": "Via Láctea",
-        "user": {
-            "name": "Christian Van der Henst",
-            "picture": "https://picsum.photos/60/60/?image=1005",
-        },
-        "timestamp": datetime.now().strftime("%b %dth, %Y - %H:%M hrs"),
-        "photo": "https://picsum.photos/800/800/?image=903",
-    },
-    {
-        "title": "Nuevo auditorio",
-        "user": {
-            "name": "Uriel (thespianartist)",
-            "picture": "https://picsum.photos/60/60/?image=883",
-        },
-        "timestamp": datetime.now().strftime("%b %dth, %Y - %H:%M hrs"),
-        "photo": "https://picsum.photos/500/700/?image=1076",
-    },
-]
-
 
 @login_required
 def list_posts(request):  # pylint: disable = unused-argument
     """
     View to lists posts
     """
-    return render(request, "posts/feed.html", {"posts": posts})
+    posts = Post.objects.all().order_by('-created')
+
+    return render(request, 'posts/feed.html', {'posts': posts})
 
 
 @login_required
