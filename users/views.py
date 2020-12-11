e8e580c8eeebb5ad60eb76401ed3d113cb4c9f36
jonathan <jonamaita@gmail.com>
2020-12-11 13:05:43 -0300

    refactor: major changes in signup view in users/views.py

--- users/views.py
+++ users/views.py
@@ -4,21 +4,13 @@ User Views
 # Importamos authenticate, login, logout
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-# Models
-# Importamos los modelos de las instancias que crearemos
-from django.contrib.auth.models import User
-# Exceptions
-# Importamos posible error al tratar de crear una instancia
-# con valor único que ya existe
-from django.db.utils import IntegrityError
+
 # Redirect nos ayudara a redireccionarnos a otro path
 from django.shortcuts import redirect, render
 
-from users.models import Profile
-
 # Forms
 # Importamos el ProfileForm que creamos anteriormente
-from users.forms import ProfileForm
+from users.forms import ProfileForm, SignupForm
 
 
 @login_required
@@ -117,49 +109,24 @@ def signup(request):
     """
     signup view
     """
+
     # Al recibir el metodo POST.
-    if request.method == "POST":
-        username = request.POST["username"]
-        password = request.POST["password"]
-        password_confirmation = request.POST["password_confirmation"]
+    if request.method == 'POST':
+        # Le enviamos los datos de request a nuestro formulario
+        form = SignupForm(request.POST)
 
-        # Confirmamos que las constraseñas sean iguales.
-        if password != password_confirmation:
-            # En caso de error volvemos a renderizar signup, pero enviamos
-            # el error
-            return render(
-                request,
-                "users/signup.html",
-                {"error": "Passwords does not match"},
-            )
+        # En caso de ser valido guarda las instancias
+        # y nos redirige al login.
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = SignupForm()
 
-        try:
-            # Creamos una instancia de User
-            user = User.objects.create_user(
-                username=username, password=password
-            )
-        except IntegrityError:
-            # En caso que username (nuestro valor unico) ya exista renderizara
-            # nuevamente signup pero enviando el error.
-            return render(
-                request,
-                "users/signup.html",
-                {"error": "Username is already exist"},
-            )
-
-        # Ya creada la instancia le pasamos los siguientes valores.
-        user.first_name = request.POST["first_name"]
-        user.last_name = request.POST["last_name"]
-        user.email = request.POST["email"]
-        # Lo guardamos en nuestra base de datos.
-        user.save()
-
-        # Creamos nuestra instacia de Profile a traves de user.
-        profile = Profile(user=user)
-        # Lo guardamos en la base de datos.
-        profile.save()
-
-        # Nos redirigimos a login para iniciar sesion con el nuevo usuario.
-        return redirect("login")
-
-    return render(request, "users/signup.html")
+    return render(
+        request=request,
+        template_name='users/signup.html',
+        context={
+            'form': form
+        }
+    )
