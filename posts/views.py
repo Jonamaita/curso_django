d539697a817b88c110ec1a4356d0aa833221a647
jonathan <jonamaita@gmail.com>
2020-12-11 11:49:25 -0300

    refactor: minor changes in create_post view

--- posts/views.py
+++ posts/views.py
@@ -5,7 +5,11 @@ Posts Views
 from datetime import datetime
 
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+
+# Forms
+from posts.forms import PostForm
 
 # Create your views here.
 
@@ -46,3 +50,29 @@ def list_posts(request):  # pylint: disable = unused-argument
     View to lists posts
     """
     return render(request, "posts/feed.html", {"posts": posts})
+
+
+@login_required
+def create_post(request):
+    """Create new post view."""
+
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.instance.user = request.user
+            form.instance.profile = request.user.profile
+            form.save()
+            return redirect('feed')
+
+    else:
+        form = PostForm()
+
+    return render(
+        request=request,
+        template_name='posts/new.html',
+        context={
+            'form': form,
+            'user': request.user,
+            'profile': request.user.profile
+        }
+    )
