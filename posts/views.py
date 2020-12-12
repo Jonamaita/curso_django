d7892528e431a8a1cbcda7e64be60b615f43cd46
jonathan <jonamaita@gmail.com>
2020-12-11 21:01:16 -0300

    refactor: delete list_posts view in posts/views.py view

--- posts/views.py
+++ posts/views.py
@@ -11,15 +11,8 @@ from posts.models import Post
 # Forms
 from posts.forms import PostForm
 
-
 # Create your views here.
 
-@login_required
-def list_posts(request):  # pylint: disable = unused-argument
-    """
-    View to lists posts
-    """
-    posts = Post.objects.all().order_by('-created')
 
     return render(request, 'posts/feed.html', {'posts': posts})
 
