6fce4101de68717985e304227c6a19044c0d4a28
jonathan <jonamaita@gmail.com>
2020-12-12 00:23:38 -0300

    refactor: delete login_view function in users/views.py

--- users/views.py
+++ users/views.py
@@ -55,37 +55,6 @@ class UserDetailView(LoginRequiredMixin, DetailView):
         return context
 
 
-def login_view(request):
-    """
-    login view
-    """
-
-    if request.method == "POST":
-        username = request.POST["username"]
-        password = request.POST["password"]
-        # El metodo authenticate tratara de contrastar el usuario con una
-        # instancia del modelo users que creamos.
-        user = authenticate(request, username=username, password=password)
-        if user:
-            # En caso de ser exitoso la autenticación creara un token de
-            # nuestro usuario para almacenarlo en memoria.
-            login(request, user)
-
-            # redireccionaremos al path con alias 'feed' que es para la url
-            # 'posts/'
-            return redirect("posts:feed")
-
-        # En caso de dar false la autenticacion volveremos a renderizar el
-        # login, pero enviando la variable 'error'.
-        return render(
-            request,
-            "users/login.html",
-            {"error": "Invalid username and password"},
-        )
-
-    return render(request, "users/login.html")
-
-
 # Creamos la funcion logout_view, y lo decoramos con
 # login_required, asi solo se ejecutara si existe una sesión.
 @login_required
